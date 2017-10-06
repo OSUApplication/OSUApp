c80cc25b6f7106dfc2192bc8c0c04158404cb9e7
prathv <prathveer.r@gmail.com>
2017-10-06 13:39:42 -0700

    Added example text

--- osuApp/src/app/app.module.ts
+++ osuApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TutorRegisterComponent } from './tutor-reg/tutor-register/tutor-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorRegisterComponent,
   ],
   imports: [
     BrowserModule
---
+++ osuApp/src/app/tutor-reg/tutor-register/tutor-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-register',
+  templateUrl: './tutor-register.component.html',
+  styleUrls: ['./tutor-register.component.css']
+})
+export class TutorRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
