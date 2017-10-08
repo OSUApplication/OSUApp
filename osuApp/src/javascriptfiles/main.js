33fe4cf15e26dd52d140e32097094a3b4d9cb8cb
Shashank <moghe.shashank007@gmail.com>
2017-10-08 03:47:36 -0700

    working register as tutor button need to add the data into db

---
+++ osuApp/src/javascriptfiles/main.js
@@ -0,0 +1,8 @@
+function test() {
+
+  var x = {};
+  x["department"]=document.getElementById('department_field').value;
+  x["subject_name"]=document.getElementById('subject_name').value;
+  x["crn"]=document.getElementById('crn').value;
+  console.log(x);
+}
