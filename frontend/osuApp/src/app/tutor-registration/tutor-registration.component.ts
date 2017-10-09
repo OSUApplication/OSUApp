440f7ab9063d5bfd2ed4c712faf3a4dceac50af9
prathv <prathveer.r@gmail.com>
2017-10-08 23:58:02 -0700

    Restrucutred frontend so angular-cli works

---
+++ frontend/osuApp/src/app/tutor-registration/tutor-registration.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutor } from './tutor';
+
+@Component({
+  selector: 'app-tutor-registration',
+  templateUrl: './tutor-registration.component.html',
+  styleUrls: ['./tutor-registration.component.css']
+})
+export class TutorRegistrationComponent implements OnInit {
+
+   public model: Tutor;
+  
+   constructor(){
+       this.model = new Tutor();
+       this.model.name = "Tutor 1";
+   }
+
+  ngOnInit() {
+
+  	  console.log("THe model value is",this.model.name);
+  }
+
+  display(name){
+  			console.log("name is",name);
+  }
+
+  log(x){
+  console.log(x);
+  }
+}
---
+++ frontend/osuApp/src/app/tutor-registration/tutor.ts
@@ -0,0 +1,3 @@
+export class Tutor {
+    name: string;
+}
