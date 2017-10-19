997a05531f77b77c55f5fb8a368a0b098bc2f62a
Harshraj Thakor <hrthakor7@gmail.com>
2017-10-19 14:49:07 -0700

    redined the code

--- backend/src/main/java/com/userController/UserServiceImpl.java
+++ backend/src/main/java/com/userController/UserServiceImpl.java
@@ -19,7 +19,6 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> getAllUser() {
-
 		Query<User> query = MongoConnection.dataStore.createQuery(User.class);
 		List<User> users = query.asList();
 		return users;
@@ -33,7 +32,6 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public ResponseEntity<?> addUser(User user) throws URISyntaxException {
-		
 		User addedUser = MongoConnection.dataStore.createQuery(User.class).field(ApplicationConstants.FIELD_EMAIL).equal(user.email).get();
 		if(addedUser == null){
 			MongoConnection.dataStore.save(user);
@@ -54,22 +52,12 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public ResponseEntity<?> updateUser(User user) throws URISyntaxException
 	{
-		User updatedUser = user;
-		
 		Query<User> query = MongoConnection.dataStore.createQuery(User.class).field("email").equal(user.email);
 		UpdateOperations<User> operations = MongoConnection.dataStore.createUpdateOperations(User.class).set("name", "update user works!!");
-	    UpdateResults t = MongoConnection.dataStore.update(query, operations);
-	    
-	    if(t.getUpdatedExisting()==true) {
+	    UpdateResults updateResults = MongoConnection.dataStore.update(query, operations);
+	    if(updateResults.getUpdatedExisting()==true) 
 			return ResponseEntity.status(HttpStatus.ACCEPTED).location(new URI(ApplicationConstants.GET_USER_END_POINT + user.email)).build();
-
-	    }
-	    else {
+	    else 
 			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).location(new URI(ApplicationConstants.GET_USER_END_POINT + user.email)).build();
-	   
-	    }
 	}
-	
-
-
 }
