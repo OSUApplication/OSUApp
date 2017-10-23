b8983174750d5db8f80df13fc6f54c70e385c182
Harshraj Thakor <hrthakor7@gmail.com>
2017-10-23 12:23:41 -0700

    changed the folder level of spring Context Class

--- backend/src/main/java/com/example/ServletInitializer.java
+++ backend/src/main/java/com/example/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.osuapp;
+package com.example;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
