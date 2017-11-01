08a35d84cc3471853ba32f0e59bc0cbd20121cf7
Harshraj Thakor <hrthakor7@gmail.com>
2017-11-01 11:48:24 -0700

    added the Course List controller and ServiceImpl

---
+++ backend/src/main/java/com/courseController/CourseController.java
@@ -0,0 +1,31 @@
+package com.courseController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.osuapp.service.CourseService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class CourseController {
+	
+	CourseService courseService;
+	Gson gson = new Gson();
+	
+	//dependency Injection
+	@Autowired
+	public CourseController(CourseService courseService) {
+		super();
+		this.courseService = courseService;
+	}
+
+	@RequestMapping("{departmentName}/getAllCourses")
+	public String getAllCourses(@PathVariable("departmentName") String departmentName) {
+		return gson.toJson(courseService.getAllCourses(departmentName));
+	}
+}
