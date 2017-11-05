1ec4d67e41fb5159843b9fe9ecea35d14eb8da10
liliw <lily.lsps@gmail.com>
2017-11-05 11:24:25 -0800

    Done: 1. add a animation icon in hompage for scrolling page down 2. add a flip animation in homepage in order to combine sign up and log in TO-DO: 1. log in validation (popover)

--- frontend/osuApp/src/app/homepage/homepage.component.ts
+++ frontend/osuApp/src/app/homepage/homepage.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
 import { RouterModule,Router } from '@angular/router';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { Student } from './student';
+import { HttpModule } from '@angular/http';
+import { Http,URLSearchParams, Headers } from '@angular/http';
+import { ToastModule, ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
+import { DataOpService} from '../data-op.service';
 import { SessionService } from '../session.service';
 
 @Component({
@@ -11,9 +19,23 @@ import { SessionService } from '../session.service';
 export class HomepageComponent implements OnInit {
 
   loggedin:boolean;
-  constructor(private router: Router, private session: SessionService) { }
+  [x: string]: any;
+  public model: Student;
+  name:string = "";
+  course:any[] = [{'id': 1,'cno':'','cname':''}];
+  id:number = 1;
+  result:Array<any> = [];
+
+  constructor(
+    private router: Router,
+    private session: SessionService,
+    private http : Http,
+    public toastr: ToastsManager,
+    vcr: ViewContainerRef,
+    private dataservice:DataOpService) { }
 
   ngOnInit() {
+    let headers = new Headers();
     if(this.session.getSession()){
       this.loggedin=true;
     }
@@ -31,6 +53,8 @@ export class HomepageComponent implements OnInit {
     this.router.navigate(['/sign-up'])
   }
 
+  // login functions
+
   login(){
      var self = this;
      this.session.setSession().then(function(){
@@ -38,5 +62,33 @@ export class HomepageComponent implements OnInit {
      self.router.navigate(['home']);
     });
   }
+
+  // animations
+
+  flip(){
+    var content = document.getElementById('f1_card');
+    content.classList.toggle('flip');
+  }
+
+  // Sign-up functions
+
+  display(){
+    var self = this;
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin','http://localhost:4200');
+    headers.append('Content-Type','application/json');
+    headers.append('Access','application/json');
+
+    this.dataservice.setRegistrationData(this).subscribe(function(resp){
+      if(resp.status == 201){
+          self.showSuccess();
+      }
+     });
+  }
+
+  showSuccess() {
+   this.router.navigate(['']);
+  }
+
 
 }
