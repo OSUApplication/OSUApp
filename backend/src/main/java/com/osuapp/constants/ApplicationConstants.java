5c07b0a2749a4766a06cd79f4901ac8302e4f0bc
Harshraj Thakor <hrthakor7@gmail.com>
2017-11-17 12:15:29 -0800

    removed /osu endpoint

--- backend/src/main/java/com/osuapp/constants/ApplicationConstants.java
+++ backend/src/main/java/com/osuapp/constants/ApplicationConstants.java
@@ -13,13 +13,13 @@ public class ApplicationConstants {
 	public static String FIELD_COURSE_OFFERING = "course_offering";
 	
 	//API Address Constants 
-	public static String GET_USER_END_POINT = "/osu/api/getUser/";
-	public static String GET_ALL_USERS = "/osu/api/getAllUsers";
-	public static String GET_USER = "/osu/api/getUser/{email:.+}";
-	public static String ADD_USER = "/osu/api/addUser";
-	public static String GET_TUTOR = "/osu/api/getTutor/{subjectName}";
-	public static String UPDATE_USER = "/osu/api/updateUser";
-	public static String GET_ALL_COURSES_BY_DEPTARTMENT = "/osu/api/{departmentName}/getAllCourses";
+	public static String GET_USER_END_POINT = "/api/getUser/";
+	public static String GET_ALL_USERS = "/api/getAllUsers";
+	public static String GET_USER = "/api/getUser/{email:.+}";
+	public static String ADD_USER = "/api/addUser";
+	public static String GET_TUTOR = "/api/getTutor/{subjectName}";
+	public static String UPDATE_USER = "/api/updateUser";
+	public static String GET_ALL_COURSES_BY_DEPTARTMENT = "/api/{departmentName}/getAllCourses";
 	
 	//Front-End localhost constant
 	public static String FRONT_END_LOCALHOST = "http://localhost:8084";
@@ -33,12 +33,12 @@ public class ApplicationConstants {
 	public static String EXPECTED_TEST_RESULT_LIST = "[{\"name\":\"Chris Rock\"}]";
 	public static String EXPECTED_TEST_RESULT_OBJECT = "{\"name\":\"Chris Rock\",\"email\":\"rockc@oregonstate.edu\"}";
 	public static String EXPECTED_TEST_COURSE_RESULT = "{\"departmentName\":\"CS\",\"courseList\":[\"CS540 DBMS\"]}";
-	public static String EXPECTED_TEST_RESULT_LOCATION = "/osu/api/getUser/rockc@oregonstate.edu";
-	public static String GENERIC_TUTOR_URL = "/osu/api/getUser/rockc@oregonstate.edu";
-	public static String GENERIC_TUTOR_FINDER_URL = "/osu/api/getTutor/CS540 DBMS";
-	public static String GENERIC_GET_ALL_COURSES_URL = "/osu/api/CS/getAllCourses";
+	public static String EXPECTED_TEST_RESULT_LOCATION = "/api/getUser/rockc@oregonstate.edu";
+	public static String GENERIC_TUTOR_URL = "/api/getUser/rockc@oregonstate.edu";
+	public static String GENERIC_TUTOR_FINDER_URL = "/api/getTutor/CS540 DBMS";
+	public static String GENERIC_GET_ALL_COURSES_URL = "/api/CS/getAllCourses";
 	public static String GENERIC_PASSWORD = "1234567";
-	public static String GENERIC_GET_TIMESLOT_URL = "/osu/api/getTimeSlot";
+	public static String GENERIC_GET_TIMESLOT_URL = "/api/schedule/timeslot/getTimeSlots/1000";
 	public static String GENERIC_GET_TIMESLOT_RESULT = "[{\"StudentId\":\"1000\",\"TutorId\":\"1000\",\"Date\":\"2-11-2012\",\"startTime\":\"5pm\",\"endTime\":\"7pm\",\"confirmed\":false}]";
 	public static String GENERIC_TIMESLOT_DATE = "2-11-2012";
 	public static String GENERIC_TIMESLOT_STDUENTID = "1000";
